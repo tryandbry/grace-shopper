f952e89fd5f1958eac7d02a23ae8e0390ae32cb8
Justin Gehr <gehrjay@gmail.com>
2017-06-06 16:45:23 -0500

    started product

--- server/api.js
+++ server/api.js
@@ -9,6 +9,7 @@ api
     .use('/auth', require('./auth'))
     .use('/users', require('./users'))
     .use('/catalog', require('./catalog'))
+    .use('/product', require('./product'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
---
+++ server/product.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const db = require('APP/db');
+const Product = db.model('product');
+const Review = db.model('review');
+
+//post is only for admin
+//delete is only for admin
+
+
+module.exports = require('express').Router()
+    .get('/:id', (req, res, next) => {
+         Product.findById(req.params.id)
+        .then(product => res.send(product))
+        .catch(next)
+    })
+    .post('/:id', (req, res, next) => {
+        Product.findById(req.params.id)
+        .then(product => product.update(req.body))
+        .then(updated => res.sendStatus(201))
+        .catch(next)
+    })
+    .get('/:id/reviews', (req, res, next) => {
+        Review.findAll( { where: user_id: req.params.id})
+        .then(reviews => res.send(reviews))
+        .catch(next)
+    })
