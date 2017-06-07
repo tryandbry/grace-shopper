5c6c24a961bb20a7d38b9220964f38dda40c27fb
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-07 16:23:46 -0500

    started payment component

---
+++ app/components/Payment.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+export default class Payment extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    const onChangeCreditCard = this.props.onCreditCard;
+    const onChangeExpiration = this.props.onChangeExpiration;
+    const onChangeCCV = this.props.onChangeCCV;
+    
+    const onChangeAddress1 = this.props.onChangeAddress1;
+    const onChangeAddress2 = this.props.onChangeAddress2;
+    const onChangeCity = this.props.onChangeCity;
+    const onChangeState = this.props.onChangeState;
+    const onChangeZip = this.props.onChangeZip;
+    const onSubmit = this.props.onSubmit;
+    const title = this.props.title;
+
+    return(
+      <div>
+	<h2>Payment Information</h2>
+	<form className="form-horizontal" onSubmit={onSubmit}>
+	  <fieldset>
+	    <legend>Credit Card Information</legend>
+	    <div className="form-group">
+	      <label>Credit Card Number</label>
+	      <input name="creditcard" onChange={onChangeCreditCard} />
+	      <label>Expiration</label>
+	      <input name="address2" onChange={onChangeAddress2} />
+	      <label>CCV</label>
+	      <input name="ccv" onChange={onChangeCCV} />
+	    </div>
+	    <div className="form-group">
+	      <button
+		type="submit"
+		className="btn btn-success">
+		Submit Payment Information
+	      </button>
+	    </div>
+	  </fieldset>
+	</form>
+	<form className="form-horizontal" onSubmit={onSubmit}>
+	  <fieldset>
+	    <legend>Billing Address</legend>
+	    <div className="form-group">
+	      <label>Address</label>
+	      <input name="address1" onChange={onChangeAddress1} />
+	      <label>Address 2</label>
+	      <input name="address2" onChange={onChangeAddress2} />
+	      <label>City</label>
+	      <input name="city" onChange={onChangeCity} />
+	      <label>State</label>
+	      <input name="state" onChange={onChangeState} />
+	      <label>Zip Code</label>
+	      <input name="zipcode" onChange={onChangeZip} />
+	    </div>
+	    <div className="form-group">
+	      <button
+		type="submit"
+		className="btn btn-success">
+		Submit Billing Address
+	      </button>
+	    </div>
+	  </fieldset>
+	</form>
+      </div>
+    )
+  }
+}
+
+/*
+export default AddressForm = (props)=>{
+  const onChangeAddress1 = props.onChangeAddress1;
+  const onChangeAddress2 = props.onChangeAddress2;
+  const onChangeCity = props.onChangeCity;
+  const onChangeState = props.onChangeState;
+  const onChangeZip = props.onChangeZip;
+  const onSubmit = props.onSubmit;
+  
+  console.log("AddressForm props:",props);
+  return (
+    <div>
+      <h2>Address Form</h2>
+    </div>
+  )
+}
+*/
+
+    /*
+    <div>
+      <form className="form-horizontal" onSubmit={onChange}>
+        <fieldset>
+	  <legend>Address</legend>
+	  <div className="form-group">
+	    <label>Address</label>
+	    <input name="address1" onChange={onChangeAddress1} />
+	    <label>Address 2</label>
+	    <input name="address2" onChange={onChangeAddress2} />
+	    <label>City</label>
+	    <input name="city" onChange={onChangeCity} />
+	    <label>State</label>
+	    <input name="state" onChange={onChangeState} />
+	    <label>Zip Code</label>
+	    <input name="zipcode" onChange={onChangeZip} />
+	  </div>
+	  <div className="form-group">
+	    <button
+	      type="submit"
+	      className="btn btn-success">
+	      Submit Address
+	    </button>
+	  </div>
+	</fieldset>
+      </form>
+    </div>
+    */
--- app/containers/CheckoutContainer.jsx
+++ app/containers/CheckoutContainer.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AddressForm from '../components/AddressForm';
+import Payment from '../components/Payment';
 
 class CheckoutContainer extends React.Component {
     constructor() {
         super();
         this.state = {
+	    flowState: 0,
 	    shipAddress1: "",
 	    shipAddress2: "",
 	    shipCity: "",
 	    shipState: "",
 	    shipZip: "",
+	    creditcard: "",
+	    expiration: "",
+	    ccv: "",
 	}
 
 	this.onSubmitShipping = this.onSubmitShipping.bind(this);
@@ -19,11 +24,17 @@ class CheckoutContainer extends React.Component {
 	this.onChangeCity = this.onChangeCity.bind(this);
 	this.onChangeState = this.onChangeState.bind(this);
 	this.onChangeZip = this.onChangeZip.bind(this);
+
+	this.onChangeCreditCard = this.onChangeCreditCard.bind(this);
+	this.onChangeExpiration = this.onChangeExpiration.bind(this);
+	this.onChangeCCV = this.onChangeCCV.bind(this);
     }
 
+    //handlers for AddressForm
     onSubmitShipping = (event)=>{
       event.preventDefault();
       console.log("submitted!",this.state);
+      this.setState({flowState: 1});
     }
 
     onChangeAddress1 = (event)=>{
@@ -46,11 +57,25 @@ class CheckoutContainer extends React.Component {
       this.setState({shipZip: event.target.value});
     }
 
+    //handlers for payment
+    onChangeCreditCard = (event)=>{
+      this.setState({creditcard: event.target.value});
+    }
+
+    onChangeExpiration = (event)=>{
+      this.setState({expiration: event.target.value});
+    }
+
+    onChangeCCV = (event)=>{
+      this.setState({ccv: event.target.value});
+    }
+
     render () {
 	console.log("rendering checkout");
         return (
 	  <div id="checkout">
 	    <h1>WELCOME TO CHECKOUT</h1>
+	    {this.state.flowState === 0 ? 
 	    <AddressForm
 	      onSubmit={this.onSubmitShipping}
 	      onChangeAddress1={this.onChangeAddress1}
@@ -60,6 +85,20 @@ class CheckoutContainer extends React.Component {
 	      onChangeZip={this.onChangeZip}
 	      title={"Shipping Address"}
 	    />
+	    : this.state.flowState === 1 ?
+	    <Payment
+	      onSubmit={this.onSubmitShipping}
+	      onChangeAddress1={this.onChangeAddress1}
+	      onChangeAddress2={this.onChangeAddress2}
+	      onChangeCity={this.onChangeCity}
+	      onChangeState={this.onChangeState}
+	      onChangeZip={this.onChangeZip}
+	      onChangeCreditCard={this.onChangeCreditCard}
+	      onChangeExpiration={this.onChangeExpiration}
+	      onChangeCCV={this.onChangeCCV}
+	      title={"Billing Address"}
+	    />
+	    : ""}
 	  </div>
         );
     }
