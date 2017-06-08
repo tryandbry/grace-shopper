bd7d5883b807e7f0f5c62de7f97f206e3db437ab
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-08 10:36:24 -0500

    adds scaffolding for step 3: review

---
+++ app/components/Review.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Review extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+
+    return(
+      <div>
+	<h2>Review</h2>
+
+      </div>
+    )
+  }
+}
+
