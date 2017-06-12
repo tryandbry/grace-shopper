f4e2a7643d27a344f094be27ebb1c9d3a52c8557
Justin Gehr <gehrjay@gmail.com>
2017-06-12 11:38:56 -0500

    messed around with home page its all centered need figure out how to do 2 per row

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-
+import { printPrice } from 'APP/utils'
 const Catalog = ({ products }) => (
-    <div>
-    <h3>Catalog</h3>
-    <div className="row">
-        {
-        products && products
-            .map(product => (
-                <div className="col-xs-4" key={ product.id }>
-                    <Link 
-                        className="thumbnail" 
-                        to={`/product/${product.id}`}
-                    >
-                        <img src={ product.image }/>
-                        <div className="caption">
-                            <h5><span>{product.name}</span></h5>
-                        </div>
-                        <small>${ product.cost }</small>
-                    </Link>
-                </div>
-            ))
-        }
-    </div>
+    <div className="center">
+        <h3>Catalog</h3>
+        <div className="row">
+            {
+                products && products
+                    .map(product => (
+                            <div className="col-lg-12" key={product.id}>
+                                <Link
+                                    className="thumbnail"
+                                    to={`/product/${product.id}`}
+                                >
+                                    <img className="images" src={product.image} />
+                                    <div className="caption">
+                                        <h5><span>{product.name}</span></h5>
+                                    </div>
+                                    <small>{printPrice(product.cost)}</small>
+                                </Link>
+                            </div>
+                    ))
+            }
+        </div>
     </div>
 )
 
