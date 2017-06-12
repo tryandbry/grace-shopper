3acec0b88ff64f77842772f60488bc8a0ac12769
Justin Gehr <gehrjay@gmail.com>
2017-06-12 12:30:35 -0500

    needs a little work but looks better for now

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import { printPrice } from 'APP/utils'
 const Catalog = ({ products }) => (
     <div>
+        <div className="center">
         <h3>Catalog</h3>
+        </div>
         <div className="row">
             {
                 products && products
                     .map(product => (
-                            <div className="col-lg-12" key={product.id}>
+                            <div className="col-lg-12 col-md-12" key={product.id}>
                                 <Link
                                     className="thumbnail"
                                     to={`/product/${product.id}`}
@@ -18,7 +20,7 @@ const Catalog = ({ products }) => (
                                     <div className="caption">
                                         <h5><span>{product.name}</span></h5>
                                     </div>
-                                    <small>{printPrice(product.cost)}</small>
+                                    <small className="hCost">price: {printPrice(product.cost)}</small>
                                 </Link>
                             </div>
                     ))
