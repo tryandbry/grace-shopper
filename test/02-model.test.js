60e411a4c9b4f1bee3fff4e6b060441fcea46799
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-13 17:16:55 -0500

    adds tests for Product model

--- test/02-model.test.js
+++ test/02-model.test.js
@@ -3,8 +3,8 @@ const {Product} = db;
 const {expect} = require('chai');
 
 describe('Back-end tests',function(){
-  beforeEach('Sync and clear database',function(){
-    return db.didSync
+  beforeEach('Sync and clear database',function(done){
+    db.didSync
     .then(()=>db.sync({force: true}))
     .then(()=>{
       return Product.create({
@@ -12,22 +12,31 @@ describe('Back-end tests',function(){
 	cost: 1243.34,
 	description: 'I am a rock',
 	inventory: 1000,
-      });
+      })
     })
+    .then(()=>done())
     .catch(error=>{
       console.log("err",error);
     });
   });
 
   describe('Sequelize models',function(){
-    describe('Product',function(){
-      it('has the expected schema definition',function(){
-	return Product.findAll()
-	.then(data=>{
-	  console.log("products found:",data)
-	});
+    let product;
 
-	//expect(Product.attributes.name).to.be.an('object');
+    beforeEach('add one Product instance',function(){
+      return Product.findOne()
+      .then(data=>{
+	product = data;
+	//console.log(product.get('name'));
+      })
+    });
+
+    describe('Product',function(){
+      it('instance has the expected schema',function(){
+	expect(product.get('name')).to.equal('rock');
+	expect(product.get('description')).to.equal('I am a rock');
+	expect(product.get('cost')).to.be.a.number;
+	expect(product.get('inventory')).to.be.an.integer;
       });
     });
   });
