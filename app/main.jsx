736918381b49738718a9acb2514e283db2ff98df
ehacinom <ehacinom@gmail.com>
2017-06-13 00:54:16 -0500

    basic about

--- app/main.jsx
+++ app/main.jsx
@@ -21,10 +21,10 @@ import Cart from './components/Cart'
 import CheckoutContainer from './containers/CheckoutContainer'
 import ProductContainer from './containers/ProductContainer'
 import UserPage from './components/UserPage'
-
+import About from './components/About'
 import Bom from './components/Bom'
 
-import { getUsersOrders } from './reducers/user'
+// import { getUsersOrders } from './reducers/user'
 import { getProducts, getCategories } from './reducers/catalog'
 import { getProduct } from './reducers/product'
 import { fetchBom } from './reducers/bom'
@@ -34,11 +34,11 @@ const onEnter = function () {
     store.dispatch(getCategories())
 }
 
-const onOrdersEnter = () => {                 // monica change this to a component will mount on userpage or figure another way but userid on state is async and not loading in time as is
-    console.log('userId? ', store.getState().auth.user.id);
-    const userId = store;
-    store.dispatch(getUsersOrders(userId));
-}
+// const onOrdersEnter = () => {                 // monica change this to a component will mount on userpage or figure another way but userid on state is async and not loading in time as is
+//     console.log('userId? ', store.getState().auth.user.id);
+//     const userId = store;
+//     store.dispatch(getUsersOrders(userId));
+// }
 
 const onProductEnter = function (nextRouterState) {
     const productId = nextRouterState.params.id;
@@ -59,12 +59,13 @@ render(
                 <Route path="/product/:id" component={ProductContainer} onEnter={onProductEnter} />
                 <Route path="/cart" component={Cart} />
                 <Route path="/checkout" component={CheckoutContainer} />
-                <Route path="/account/:userId" component={UserPage} onEnter={onOrdersEnter} />
+                <Route path="/account" component={UserPage} />
             </Route>
             <Route path="/bom/:id" component={Bom} onEnter={fetchBomOnEnter} />
             <Route path='/login' component={LoginPage} />
             <Route path='/signup' component={SignUpContainer} />
             <Route path='/logout' component={Logout} />
+            <Route path='/about' component={About} />
             <Route path='*' component={NotFound} />
         </Router>
     </Provider>,
