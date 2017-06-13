c929f3c26d82921f0c44aaf9eb48b4afb2a2eeb7
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-13 18:53:23 -0500

    switches Sidebar to radio buttons

--- app/components/FilterCategory.jsx
+++ app/components/FilterCategory.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getSelectedCategory } from '../reducers/catalog';
-import { Link } from 'react-router';
+//import { Link } from 'react-router';
 
-const FilterCategory = (props) => {
-    const categories = props.categories;
-    const selectCategory = props.selectCategory;
+const FilterCategory = ({categories, selectCategory}) => {
+    //const categories = props.categories;
+    //const selectCategory = props.selectCategory;
 
-    console.log("Categories: ",categories);
+    //console.log("Categories: ",categories);
 
     return (
         <div id="filtercategories">
@@ -23,15 +23,15 @@ const FilterCategory = (props) => {
 	      />all
 	    </label>
 	  </div>
-	  {categories && categories.map(category =>
-	    <div className="radio">
+	  {categories && categories.map((category,i) =>
+	    <div className="radio" key={i}>
 	      <label>
 		<input 
 		  type="radio"
 		  name="categoryRadios"
 		  id={`category${category.name}`}
                   value={category.id}
-                  onChange={()=>selectCategory(category.id)}
+                  onChange={()=>selectCategory(category.name)}
 		/>{category.name}
 	      </label>
 	    </div>
