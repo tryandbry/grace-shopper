ae121f328d711ba2ec50aff6a3344b3da21aaf93
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-13 19:06:27 -0500

    re-factored FilterCategory.jsx

--- app/components/FilterCategory.jsx
+++ app/components/FilterCategory.jsx
@@ -3,38 +3,41 @@ import { connect } from 'react-redux';
 import { getSelectedCategory } from '../reducers/catalog';
 //import { Link } from 'react-router';
 
+
+const CategoryItem = ({category,i,selectCategory})=>{
+
+  return (
+    <div className="radio" key={i}>
+      <label>
+	<input 
+	  type="radio"
+	  name="categoryRadios"
+	  id={`category${category.name}`}
+	  value={category.id}
+	  onChange={()=>selectCategory(category.name)}
+	/>{category.name}
+      </label>
+    </div>
+  )
+}
+
+
 const FilterCategory = ({categories, selectCategory}) => {
     //const categories = props.categories;
     //const selectCategory = props.selectCategory;
 
     //console.log("Categories: ",categories);
+    const allProps = {
+      name: 'All',
+      id: 'all',
+    };
 
     return (
         <div id="filtercategories">
 	  <h3>Categories</h3>
-	  <div className="radio">
-	    <label>
-	      <input 
-	        type="radio"
-		name="categoryRadios"
-		id="categoryAll"
-		value="all"
-		onChange={()=>selectCategory('all')}
-	      />all
-	    </label>
-	  </div>
+	  <CategoryItem category={allProps} i={10} selectCategory={selectCategory} />
 	  {categories && categories.map((category,i) =>
-	    <div className="radio" key={i}>
-	      <label>
-		<input 
-		  type="radio"
-		  name="categoryRadios"
-		  id={`category${category.name}`}
-                  value={category.id}
-                  onChange={()=>selectCategory(category.name)}
-		/>{category.name}
-	      </label>
-	    </div>
+	    <CategoryItem category={category} i={i} selectCategory={selectCategory} />
 	  )}
         </div>
     )
